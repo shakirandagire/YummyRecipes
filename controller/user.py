3cf7d98c2ede9e23ba5b1443fc886d5cd9d39138
Shakira <ndagire.shakira@gmail.com>
2017-11-08 16:57:09 +0300

    Updates to the templates for the application

--- controller/user.py
+++ controller/user.py
@@ -1,43 +1,22 @@
 class User:
     
-    users_store = {}
+    users_store ={}
    
-    def __init__(self,firstname,lastname, email, password):
-        self.firstname = firstname
-        self.lastname = lastname
-        self.email = email
+    def __init__(self,username, password):
+        self.username = username
         self.password = password
-        self.categories = {}
-     
-
+        
     @classmethod
-    def login(cls,first_name, password):
-        if first_name == cls.users_store[first_name].firstname:
+    def login(cls,username, password):
+        if username == cls.users_store[username].username:
             return True
         else:
             return False
         
-
     @classmethod   
-    def signup(cls,firstname,lastname, email, password):
-        new_user = cls(firstname,lastname,email,password)
-        cls.users_store[new_user.firstname] =  new_user
+    def signup(cls,username, password):
+        new_user = cls(username,password)
+        cls.users_store[new_user.username] =  new_user
         
 
-    def add_category(self, category):
-        if category not in self.categories.values():
-            self.categories[category.name]= category
-
-       
-    def delete_category(self, category_name):
-        if category_name in self.categories:
-             self.categories[category_name] = None
-
-        else:
-             return ValueError   
-
-
-    def view_categories(self):
-        return self.categories.values()
     
-
